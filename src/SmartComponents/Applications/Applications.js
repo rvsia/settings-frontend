28f52ad844e6ac7ebc342cb4602159aef6043474
tahmidefaz <tefaz@redhat.com>
2019-11-08 09:12:25 -0500

    taking out the testApp route

--- src/SmartComponents/Applications/Applications.js
+++ src/SmartComponents/Applications/Applications.js
@@ -1,24 +1,18 @@
-import { Route, Switch } from 'react-router-dom';
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { PageHeader, PageHeaderTitle } from '@redhat-cloud-services/frontend-components';
-import asyncComponent from '../../Utilities/asyncComponent';
 
 class Applications extends Component {
 
     render() {
         const appName = this.props.match.params.id;
 
-        const TestApp = asyncComponent(() => import(/* webpackChunkName: "TestApp" */ './TestApp'));
         return (
             <React.Fragment>
                 <PageHeader>
                     <PageHeaderTitle title='Applications Settings'/>
                     <p>{ `Settings for ${ appName }` }</p>
                 </PageHeader>
-                <Switch>
-                    <Route exact path='/applications/testApp' component={ TestApp }/>
-                </Switch>
             </React.Fragment>
         );
     }
